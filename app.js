9d93d26b8afe26609668aeea2b0a59cfa4669d12
Alief <Aliefiayasin@gmail.com>
2022-04-17 08:57:59 +0200

    add login

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const router = express.Router()
 const app = express()
 const port = 3000
 let posts = require('./db/posts.json') // import
+const users = posts
 
 app.set('view engine', 'ejs')
 
@@ -14,6 +15,52 @@ app.get('/', (req, res) => {
 
 app.get('/game', (req, res) => {
   res.render('game')
+})
+
+app.get('/greet', (req, res) => {
+  const name = req.query.name || 'Void'
+  res.render('greet', {name})
+})
+
+app.get('/users', (req, res) => {
+  res.json(users)
+})
+
+app.get('/register', (req, res) => {
+  res.render('register')
+})
+
+app.post('/register', (req, res) => {
+  const {username, email, password} = req.body
+  users.push({username, email, password})
+  res.redirect('/')
+})
+
+app.get('/login', (req, res) => {
+  res.render('login')
+})
+
+app.get('/test', (req, res) => {
+  // for (var i = 0; i <= users.length; i++){
+    res.status(200).send(`You're successfully login, ${users[0].username} ${users[0].fullname} ${users[0].password}`)
+  // }  
+})
+
+app.post('/login', (req, res) => {
+  const {username, password} = req.body
+  let loginStatus = false
+   for (var i = 0; i < users.length; i++){
+     if (username === users[i].username && password === users[i].password){
+        loginStatus = true;
+        break;
+      }
+   }
+
+   if(loginStatus){     
+    res.status(200).send(`You're successfully login, ${users[i].fullname}`)
+   }else{     
+    res.status(200).send(`Login failed`)
+   }
 })
 
 app.get('/api/v1/posts', (req,res) =>{
